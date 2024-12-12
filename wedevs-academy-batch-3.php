1b843a57204beddc0a6b400f0ef4141001e8e8f8
Abs-Anik <abubakkarsiddik.cse@com>
2024-12-12 18:22:56 +0600

    update

--- wedevs-academy-batch-3.php
+++ wedevs-academy-batch-3.php
@@ -17,6 +17,7 @@ class Academy_Batch_Three {
     private function __construct() {
         add_filter('the_content', array($this, 'the_content_callback'));
         add_action('wp_footer', array($this, 'wp_footer'));
+        add_filter('body_class', array($this, 'body_class'), 10, 2);
     }
 
     public static function get_instance() {
@@ -27,20 +28,33 @@ class Academy_Batch_Three {
         self::$instance = new self();
         return self::$instance;
     }
-
     public function the_content_callback($content) {
         // return "Hi Anik";
         //return $content; // filter hook can change the data
+        $is_show = apply_filters('academy_show_post_content_qr_code', true);
+        if ( ! $is_show ) {
+            return $content;
+        }
+        
         $url = get_the_permalink();
-        $image = '<p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$url.'" alt="qrcode"></p>';
+        $custom_class = implode( " ", apply_filters('qr_code_css_classes', array()) );
+        $image = '<p><img class="'.$custom_class.'" src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$url.'" alt="qrcode"></p>';
         $content .= $image;
         return $content;
     }
 
     public function wp_footer() {
+        
         $url = home_url();
         $image = '<p><img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data='.$url.'" alt="qrcode"></p>';
         echo $image;
+        do_action('before_footer_qr_code', array(1,2,3), 'Mr. Anik');
+    }
+
+    public function body_class($classes, $css_class) {
+        print_r( $classes);
+        $classes[] = 'my-image-class';
+        return $classes;
     }
 }
 
